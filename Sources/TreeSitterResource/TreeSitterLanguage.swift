b3215a59beddef45517df55ab98784be18e0a99d
Matt Ricketson <matt@bitrig.com>
2026-03-23 21:58:46 -0700

    Re-add tree-sitter language support for bash, c, json, and markdown (#2)
    Restores the TreeSitterLanguage enum cases, Package.swift target
    definitions, and source directories that were removed in e467787
    and 6b14467.

--- Sources/TreeSitterResource/TreeSitterLanguage.swift
+++ Sources/TreeSitterResource/TreeSitterLanguage.swift
@@ -2,14 +2,34 @@ import Foundation
 
 import TreeSitter
 
+import TreeSitterBash
+import TreeSitterBashQueries
+import TreeSitterC
+import TreeSitterCQueries
+import TreeSitterJSON
+import TreeSitterJSONQueries
+import TreeSitterMarkdown
+import TreeSitterMarkdownQueries
 import TreeSitterSwift
 import TreeSitterSwiftQueries
 
 public enum TreeSitterLanguage: CaseIterable, Hashable {
+    case bash
+    case c
+    case json
+    case markdown
     case swift
 
     public var parser: OpaquePointer {
         switch self {
+        case .bash:
+            tree_sitter_bash()
+        case .c:
+            tree_sitter_c()
+        case .json:
+            tree_sitter_json()
+        case .markdown:
+            tree_sitter_markdown()
         case .swift:
             tree_sitter_swift()
         }
@@ -17,6 +37,14 @@ public enum TreeSitterLanguage: CaseIterable, Hashable {
 
     public var highlightQueryURL: URL? {
         switch self {
+        case .bash:
+            TreeSitterBashQueries.Query.highlightsFileURL
+        case .c:
+            TreeSitterCQueries.Query.highlightsFileURL
+        case .json:
+            TreeSitterJSONQueries.Query.highlightsFileURL
+        case .markdown:
+            TreeSitterMarkdownQueries.Query.highlightsFileURL
         case .swift:
             TreeSitterSwiftQueries.Query.highlightsFileURL
         }
@@ -24,6 +52,14 @@ public enum TreeSitterLanguage: CaseIterable, Hashable {
 
     public var localsQueryURL: URL? {
         switch self {
+        case .bash:
+            nil
+        case .c:
+            nil
+        case .json:
+            nil
+        case .markdown:
+            nil
         case .swift:
             TreeSitterSwiftQueries.Query.localsFileURL
         }
